0d50c52d9a91a08bc06ac6c7e050a55baab11e4a
KakRei <ra.raihanaditya@gmail.com>
2024-01-22 05:58:11 +0700

    edit

---
+++ script.js
@@ -0,0 +1,23 @@
+function faqButton1() {
+    document.getElementById("button-1").classList.toggle("showText");
+}
+
+function faqButton2() {
+    document.getElementById("button-2").classList.toggle("showText");
+}
+
+function faqButton3() {
+    document.getElementById("button-3").classList.toggle("showText");
+}
+
+function faqButton4() {
+    document.getElementById("button-4").classList.toggle("showText");
+}
+
+function faqButton5() {
+    document.getElementById("button-5").classList.toggle("showText");
+}
+
+function faqButton6() {
+    document.getElementById("button-6").classList.toggle("showText");
+}
