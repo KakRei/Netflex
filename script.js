abcad5a84c9aabd118ddd99707964dcc4b9ae0b8
raihan <ra.raihanaditya@gmail.com>
2024-01-31 05:53:27 +0700

    idk

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 function faqButton1() {
-    document.getElementById("button-1").classList.toggle("showText");
+    var x = document.getElementById("button-1");
+    if (x.className === "faq_text") {
+        x.className += "showText";
+    } else {
+        x.className = "faq_text";
+    }
 }
 
 function faqButton2() {
-    document.getElementById("button-2").classList.toggle("showText");
+    var x = document.getElementById("button-2");
+    if (x.className === "faq_text") {
+        x.className += "showText";
+    } else {
+        x.className = "faq_text";
+    }
 }
 
 function faqButton3() {
-    document.getElementById("button-3").classList.toggle("showText");
+    var x = document.getElementById("button-3");
+    if (x.className === "faq_text") {
+        x.className += "showText";
+    } else {
+        x.className = "faq_text";
+    }
 }
 
 function faqButton4() {
-    document.getElementById("button-4").classList.toggle("showText");
+    var x = document.getElementById("button-4");
+    if (x.className === "faq_text") {
+        x.className += "showText";
+    } else {
+        x.className = "faq_text";
+    }
 }
 
 function faqButton5() {
-    document.getElementById("button-5").classList.toggle("showText");
+    var x = document.getElementById("button-5");
+    if (x.className === "faq_text") {
+        x.className += "showText";
+    } else {
+        x.className = "faq_text";
+    }
 }
 
 function faqButton6() {
-    document.getElementById("button-6").classList.toggle("showText");
+    var x = document.getElementById("button-6");
+    if (x.className === "faq_text") {
+        x.className += "showText";
+    } else {
+        x.className = "faq_text";
+    }
 }
+
